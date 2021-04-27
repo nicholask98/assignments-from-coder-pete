c40201f7a626bfd9dd5e4e0b87d280ae31685f8f
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-04-27 14:40:19 -0700

    complete

--- pg2-2/2/pg2-2-2.py
+++ pg2-2/2/pg2-2-2.py
@@ -18,21 +18,30 @@ screen = pygame.display.set_mode((640, 480), 0, 32)
 background = pygame.image.load(bkg_img).convert()
 mouse_csr = pygame.image.load(pizza_img).convert_alpha()
 
-## Set FPS variable and initialize Clock Object
-fpsClock = pygame.time.Clock()
-FPS = 40
+
 
 ## Have the pizza start on the upper left corner of the screen.
 posX = 0
 posY = 0
 
-clock = pygame.time.Clock()     ## The clock object is used to have consistent timing on all computers.
+## Set FPS variable and initialize Clock Object
+fpsClock = pygame.time.Clock() ## The clock object is used to have consistent timing on all computers.
+FPS = 20
+frame_counter = 0
+
 speedX = 100                    ## This makes the speed adjustable
 speedY = 300
 
 ## Main loop for the program - Includes logic to cleanup when user
 ##  presses close (red X)
 while True:
+    pygame.display.set_caption(str(FPS))
+    frame_counter += 1
+    if frame_counter == 20:
+        frame_counter = 0
+        FPS += 1
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -54,4 +63,18 @@ while True:
 
     ## Update the screen
     pygame.display.update()
-    fpsClock.tick(2000)
+    fpsClock.tick(FPS)
+
+
+    '''
+    1.
+    - a frame is a single still image in an animation. Many frames that are slightly altered and viewed in rapid succession create the illusion of movement
+    - FPS is frames per second. It is a number that corresponds to the amount of [still images](frames) viewed [for each](per) interval of 1 second of time that passes
+    - pygame.time.Clock() is an Object in the 'time' module located inside the 'pygame' module. This type of Object can track the amount of time since its initialization and has many methods that can be used to manipulate, alter, and utilize time.
+    
+    2. There must be an initialized Clock object, an FPS variable, and a regular call to the Object's tick() method including the FPS variable as a parameter. Alternatively one could just include an integer as a tick() argument and not include an FPS variable, but this would prove to be a less flexible option.
+    3. tick() method
+    4. Hertz
+    5. The game runs too slow.
+    6. The Clock.tick() method is used to track the amount of time since the last call to .tick() for that instance of the object. This method pauses the program for the amount of time required to achieve the FPS that is included as its parameter.
+    '''
